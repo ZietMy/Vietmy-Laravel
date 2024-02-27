41d2b5ad64e5708305d856c334a9e7eb1ac1db68
Viet My <Ziettmy@gmail.com>
2024-02-27 22:53:55 +0700

    Laravel-Bai10

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -16,32 +16,6 @@ class CategoryController extends Controller
     }
     // hiển thị danh sách chuyên mục pt get
     public function index(Request $request){
-        // if (isset($_GET['id'])){
-        //     echo $_GET['id'];
-        // }
-        // $dataRequest = $request->all();
-        // dd($dataRequest);
-        // $path = $request->path();
-        // echo $path;
-        // $url = $request->url();
-        // $fullUrl = $request->fullUrl();
-        // $method = $request->method();
-
-        // $ip = $request->ip();
-        // echo 'ip'.$ip;
-        // $server = $request ->server();
-        // dd($server);
-
-        // $header = $request->header();
-        // dd($header['']);
-
-        // $id = $request->input('id');
-
-        // $input= $request->input();
-        // dd($input);
-
-        $id=$request->input('id.1.name');
-        dd($id);
         return view('clients/category/list');
     }
     
@@ -58,12 +32,33 @@ class CategoryController extends Controller
     }
     // Thêm dữ liệu vào chuyên mục phương thức post
     public function addCategory(Request $request){
-        $allData = $request->all();
-        dd($allData);
+        if($request->has('category_name')){
+            $request->flash();
+            // dd($cateName);
+          return  redirect(route('category.add'));
+        }
+        else {
+            return 'Không có category name';
+        }
+        $old = $request->old('category_name');
         return view('clients/category/add');
     }
     // Xóa dữ liệu bằng pt delete
     public function deleteCategory($id){
+    }
+    public function getFile(){
+        return view('clients/category/file');
+    }
+    public function Handlefile(Request $request){
+       // $file = $request->file();
+       
+        // dd($file);
+        if ($request->hasFile('photo')){
+            $file = $request->file();
+        }
+        else{
+            return "chọn file";
+        }
     }
 
 }
